84f3801c99ccd11308c6f384ddc8a41b8d486bed
dacamokid <dendyrabbi@gmail.com>
2025-10-04 09:43:03 +0700

    pertemuan 4

---
+++ resources/views/about.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Tentang Kami - LaraPress</title>
+</head>
+<body>
+    <h1>Tentang LaraPress</h1>
+    <p>LaraPress adalah sebuah proyek blog sederhana yang dibuat untuk mempelajari dasar-dasar framework Laravel 12.</p>
+    <a href="/contact">Lihat Halaman kontak Kami</a>
+    <a href="/">Lihat Halaman utama Kami</a>
+</body>
+</html>
---
+++ resources/views/kontak.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>kontak Kami - LaraPress</title>
+</head>
+<body>
+    <h1>Tentang kontak LaraPress</h1>
+    <p>kontak kami adalah sebuah proyek blog sederhana yang dibuat untuk mempelajari dasar-dasar framework Laravel 12.</p>
+    <a href="/">Lihat Halaman Utama Kami</a>
+    <a href="/tentang-kami">Lihat Halaman Tentang Kami</a>
+</body>
+</html>
